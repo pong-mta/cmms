c2a678bcbb64eb7a8bac3493279d74da9ad99b59
juki3r <deepong25@gmail.com>
2026-08-26 10:58:15 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\DocumentController;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
