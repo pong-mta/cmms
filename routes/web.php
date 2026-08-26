dee21e4e0e669b4c83e5765a6e68a026769ecb46
juki3r <deepong25@gmail.com>
2026-08-26 10:56:53 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -30,48 +30,6 @@ Route::middleware(['auth'])->group(function () {
             ]),
         ]);
     })->name('dashboard');
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | DOCUMENTS
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/documents',
-        [DocumentController::class, 'index']
-    )->name('documents.index');
-
-    Route::get(
-        '/documents/create',
-        [DocumentController::class, 'create']
-    )->name('documents.create');
-
-    Route::post(
-        '/documents',
-        [DocumentController::class, 'store']
-    )->name('documents.store');
-
-    Route::get(
-        '/documents/{document}/edit',
-        [DocumentController::class, 'edit']
-    )->name('documents.edit');
-
-    Route::put(
-        '/documents/{document}',
-        [DocumentController::class, 'update']
-    )->name('documents.update');
-
-
-    //EXCEL IMPORT AND OPEN TO EDITOR
-    Route::get('/templates/import/excel', function () {
-        return Inertia::render('templates/import-excel');
-    })->name('templates.import.excel');
-
-    Route::get('/templates/import/excel/editor', function () {
-        return Inertia::render('templates/excel-editor');
-    })->name('templates.excel.editor');
 });
 
 require __DIR__ . '/settings.php';
