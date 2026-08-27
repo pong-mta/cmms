e2bf124e05db72266d9870950ac39b138fc0b004
juki3r <deepong25@gmail.com>
2026-08-28 00:41:34 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -342,31 +342,14 @@ Route::middleware(['auth'])->group(function () {
 
     // PREVENTIVE MAINTENANCE
 
-    // Route::get(
-    //     '/maintenance/preventive',
-    //     [
-    //         PreventiveMaintenanceScheduleController::class,
-    //         'index',
-    //     ]
-    // )->name(
-    //     'maintenance.preventive'
-    // );
     Route::get(
-        '/maintenance/preventive',
-        function () {
-            return 'PREVENTIVE ROUTE WORKS';
-        }
+        '/preventive-maintenance',
+        [
+            PreventiveMaintenanceScheduleController::class,
+            'index',
+        ]
     )->name(
         'maintenance.preventive'
-    );
-
-    Route::get(
-        '/pm-test',
-        function () {
-            return 'PM TEST WORKS';
-        }
-    )->name(
-        'pm.test'
     );
 
     Route::post(
