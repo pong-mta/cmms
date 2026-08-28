007c3e3a0e9d4689a6eb0474ec17f6fad7a5e043
PONG-MTA <deepong25@gmail.com>
2026-08-28 21:34:33 +0800

    aa

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -43,7 +43,12 @@ class HandleInertiaRequests extends Middleware
             'name' => config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
-                'user' => $request->user(),
+                'user' => $request->user()
+                    ? $request->user()->load([
+                        'department:id,name,code',
+                        'roles:id,name',
+                    ])
+                    : null,
             ],
         ]);
     }
